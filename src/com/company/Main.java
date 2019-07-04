b5f504cc563ebb35f06c30139f6f2eb6891b5fe5
ducxo98 <47741087+ducxo98@users.noreply.github.com>
2019-07-04 08:16:05 +0700

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+     MyClass ob = new MyClass();
+        System.out.println(ob.i +"\r\n"+ ob.c +"\r\n"+ob.d+"\r\n"+ob.f+"\r\n"+ ob.b + "\r\n"+ ob.s);
+    }
+}
---
+++ src/com/company/MyClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyClass {
+    int i = 10;
+    float f = 20.5f;
+    double d = 20.5;
+    boolean b = true;
+    char c = 'a';
+    String s = "Hà Nội";
+}
